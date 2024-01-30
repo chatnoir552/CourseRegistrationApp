4b646d7c335d9fc93dcf8ab62c46a3d39bef4c23
chatnoir552 <kaito5522@icloud.com>
2024-01-30 20:44:03 +0900

    単位設定削除での依存関係エラーを解決

--- app/models/maincredit.rb
+++ app/models/maincredit.rb
@@ -1,5 +1,6 @@
 class Maincredit < ApplicationRecord
   belongs_to :user
+  has_many :bookmarks, dependent: :destroy
   has_many :subcredits, dependent: :destroy
   accepts_nested_attributes_for :subcredits, allow_destroy: true
   
